5f65113df6fd8ac21cd54c075c40bcc851e0023a
Mrcannonbolt <mateuszw260@gmail.com>
2025-01-24 21:45:26 +0100

    Last update

--- src/main/java/com/hospital/ListOfObjectsController.java
+++ src/main/java/com/hospital/ListOfObjectsController.java
@@ -1,14 +1,21 @@
 package com.hospital;
-
+import com.hospital.Appointments.Appointment;
+import com.hospital.Medicaments_and_Equipment.Prescription;
+import com.hospital.Space_Availability.Bed;
+import com.hospital.Space_Availability.Department;
+import com.hospital.Space_Availability.Room;
+import com.hospital.Staff_and_patients.Patient;
 import com.hospital.Staff_and_patients.Staff;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.collections.FXCollections;
+import javafx.collections.transformation.FilteredList;
 
 public class ListOfObjectsController {
 
@@ -18,15 +25,48 @@ public class ListOfObjectsController {
     private ListView<HospitalEntity> listOfObjects;
 
     @FXML
+    private ComboBox<Object> sortObjects;
+
+    @FXML
     private MainController mainController;
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
 
+    FilteredList<HospitalEntity> filteredItems = new FilteredList<>(FXCollections.observableList(App.listOfObjects), entity -> true); // Początkowo pokazujemy wszystko
+
     public void initialize(Staff staff){
         this.staff = staff;
         listOfObjects.setItems(FXCollections.observableList(App.listOfObjects));
+        sortObjects.setItems(FXCollections.observableArrayList("Wszystko","Pacjenci", "Pracownicy", "Łóżka", "Pokoje", "Departamenty", "Recepty", "Wizyty"));
+        sortObjects.setValue("Wszystko");
+        listOfObjects.setItems(filteredItems);
+        
+       
+
+        // Dodajemy listener do ComboBox, aby filtrować na podstawie wybranego typu
+        sortObjects.valueProperty().addListener((observable, oldValue, newValue) -> {
+            filteredItems.setPredicate(item -> {
+                if (newValue.equals("Pacjenci")) {
+                    return item instanceof Patient;
+                } else if (newValue.equals("Pracownicy")) {
+                    return item instanceof Staff;
+                } else if (newValue.equals("Łóżka")) {
+                    return item instanceof Bed;
+                } else if (newValue.equals("Pokoje")) {
+                    return item instanceof Room;
+                } else if (newValue.equals("Departamenty")) {
+                    return item instanceof Department;
+                } else if (newValue.equals("Recepty")) {
+                    return item instanceof Prescription;
+                } else if (newValue.equals("Wizyty")) {
+                    return item instanceof Appointment;
+                }
+                return true; // Jeśli wybrano "Wszystko", pokazujemy wszystkie elementy
+            });
+        });
+        
 
         listOfObjects.setCellFactory(listView -> new ListCell<>() {
             @Override
@@ -50,10 +90,12 @@ public class ListOfObjectsController {
             }
         });
     }
+    
+
     public void removeObject(HospitalEntity item){
         try{
         item.removeObject(false);
-        listOfObjects.setItems(FXCollections.observableList(App.listOfObjects));
+        backMenu();
         }
         catch (IllegalStateException e) {
             showAlert("Błąd", e.getMessage(), Alert.AlertType.ERROR);
